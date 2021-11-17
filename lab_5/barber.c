b0ccad883b1fbe01742c3db20b3ec0a41c315c4a
Cliche <745356675@qq.com>
2021-11-17 17:59:09 +0800

    -

--- lab_5/barber.c
+++ lab_5/barber.c
@@ -17,9 +17,6 @@ int main(int argc, char *argv[]) {
     sofa_quest_flg = IPC_CREAT | 0644;
     sofa_quest_key = 111;
     sofa_quest_id = set_msq(sofa_quest_key, sofa_quest_flg);
-    sofa_respond_flg = IPC_CREAT | 0644;
-    sofa_respond_key = 112;
-    sofa_respond_id = set_msq(sofa_respond_key, sofa_respond_flg);
     // 建立barber队列, 可容纳4人
     barber_quest_flg = IPC_CREAT | 0644;
     barber_quest_key = 211;    
@@ -34,45 +31,41 @@ int main(int argc, char *argv[]) {
     account_respond_flg = IPC_CREAT | 0644;
     account_respond_key = 214;    
     account_respond_id = set_msq(account_respond_key, account_respond_flg);
-    // 建立顾客信号量
-    customer_key = 311;    
-    sem_flg = IPC_CREAT | 0644;    
-    sem_val = 0;    
-    customer_sem = set_sem(customer_key, sem_val, sem_flg);    
+
     // 建立账本信号量
     account_key = 312;    
     sem_flg = IPC_CREAT | 0644;
     sem_val = 1;    
     account_sem = set_sem(account_key, sem_val, sem_flg);
-    // 建立座椅信号量
-    chair_key = 313;
-    sem_flg = IPC_CREAT | 0644;
-    sem_val = 3;
-    chair_sem = set_sem(chair_key, sem_val, sem_flg);
+    // 共享内存
+    buff_key = 101;
+    buff_num = STRSIZE + 1;
+    shm_flg = IPC_CREAT | 0644;
+    buff_ptr = (char *)set_shm(buff_key, buff_num, shm_flg);
 
+    printf("理发师 %d 睡眠...\n", barber_num);
     // barber
     while (1) {
-        int bpid = getpid();
-        printf("barber %d is sleeping\n");
-        // printf("barber %d is sleeping\n", bpid);
-        // 设置阻塞状态
-        barber_quest_flg = 0;
-        // 此时msgtype可以 = 0, 只接收第一条信息
-        if (msgrcv(barber_quest_id, &msg_arg, sizeof(msg_arg), 0, barber_quest_flg) >= 0) {
-            // quest from sofa message queue     
-            // msgflg = 0, 阻塞状态          
-            msgsnd(barber_respond_id, &msg_arg, sizeof(msg_arg), 0);
-            // printf("barber %d is serving customer %d\n", bpid, msg_arg.mid);
-            printf("barber %d is serving customer %d\n", barber_num, msg_arg.mid);
+        if (msgrcv(barber_quest_id, &msg_arg, sizeof(msg_arg), HAIRQUEST, IPC_NOWAIT) >= 0) {     
+            buff_ptr[SOFA]++;
+            buff_ptr[CHAIR]--;  
+            printf("理发师 %d 为顾客 %d 理发中...\n", barber_num, msg_arg.mid);
             sleep(rate);
-            // 账本信号量互斥             
-            sem_wait(account_sem);                
-            // printf("barber %d is charging customer %d\n", bpid, msg_arg.mid);
-            printf("barber %d is charging customer %d\n", barber_num, msg_arg.mid);
+            printf("顾客 %d 理发完毕\n", msg_arg.mid);
+            // 理发师通知顾客理发完成, 让顾客找理发师收费
+            msg_arg.mtype = CUT_FINISHED;
+            msgsnd(barber_respond_id, &msg_arg, sizeof(msg_arg), 0);
+            printf("理发师 %d 睡眠...\n", barber_num);
+        }
+        if (msgrcv(account_quest_id, &msg_arg, sizeof(msg_arg), ACCOUNTQUEST, IPC_NOWAIT) >= 0) {
+            sem_wait(account_sem);
+            printf("理发师 %d 给顾客 %d 结账\n", barber_num, msg_arg.mid);
+            sleep(rate);
+            printf("顾客 %d 结账完毕\n", msg_arg.mid);
+            msg_arg.mtype = ACC_FINISHED;
+            msgsnd(account_respond_id, &msg_arg, sizeof(msg_arg), 0);
             sem_signal(account_sem);
-        } else {
-            // printf("barber %d is sleeping\n", bpid);
-            printf("barber %d is sleeping\n", barber_num);
+            printf("理发师 %d 睡眠...\n", barber_num);
         }
     }
 
--- lab_5/customer.c
+++ lab_5/customer.c
@@ -17,9 +17,6 @@ int main(int argc, char *argv[]) {
     sofa_quest_flg = IPC_CREAT | 0644;
     sofa_quest_key = 111;
     sofa_quest_id = set_msq(sofa_quest_key, sofa_quest_flg);
-    sofa_respond_flg = IPC_CREAT | 0644;
-    sofa_respond_key = 112;
-    sofa_respond_id = set_msq(sofa_respond_key, sofa_respond_flg);
     // 建立barber队列, 可容纳4人
     barber_quest_flg = IPC_CREAT | 0644;
     barber_quest_key = 211;    
@@ -33,26 +30,23 @@ int main(int argc, char *argv[]) {
     account_quest_id = set_msq(account_quest_key, account_quest_flg);
     account_respond_flg = IPC_CREAT | 0644;
     account_respond_key = 214;    
-    account_respond_id = set_msq(account_respond_key, account_respond_flg);
-    // 建立顾客信号量
-    customer_key = 311;    
-    sem_flg = IPC_CREAT | 0644;    
-    sem_val = 0;    
-    customer_sem = set_sem(customer_key, sem_val, sem_flg);    
+    account_respond_id = set_msq(account_respond_key, account_respond_flg);  
     // 建立账本信号量
     account_key = 312;    
     sem_flg = IPC_CREAT | 0644;
     sem_val = 1;    
     account_sem = set_sem(account_key, sem_val, sem_flg);
-    // 建立座椅信号量
-    chair_key = 313;
-    sem_flg = IPC_CREAT | 0644;
-    sem_val = 3;
-    chair_sem = set_sem(chair_key, sem_val, sem_flg);
-    // 沙发上人数
-    int sofa_count = 0;    
-    // 等待室人数
-    int wait_count = 0;
+    // 共享内存
+    buff_key = 101;
+    buff_num = STRSIZE + 1;
+    shm_flg = IPC_CREAT | 0644;
+    buff_ptr = (char *)set_shm(buff_key, buff_num, shm_flg);
+    // 沙发上剩余座位
+    buff_ptr[SOFA] = 4;   
+    // 等待室剩余座位
+    buff_ptr[ROOM] = 13;
+    // 椅子剩余座位
+    buff_ptr[CHAIR] = 3;
     // 顾客id 
     int id = -1;
 
@@ -61,50 +55,57 @@ int main(int argc, char *argv[]) {
         id++;
         msg_arg.mid = id;
         msg_arg.mtype = id + 1;
-        // 来了新customer
-        if (sofa_count < 4) {
+        // 来了新customer, 先让新顾客坐下, 然后处理请求
+        if (buff_ptr[SOFA] > 0) {
             // 沙发有空位
-            if (wait_count > 0) {
-                // 等待室有人
-                sofa_quest_flg = IPC_NOWAIT;
-                // sofa收到waiting room请求进入sofa, 请求是非阻塞的, 这个请求一定是room中时间最长的customer的quest
-                msgrcv(sofa_quest_id, &msg_arg, sizeof(msg_arg), 0, sofa_quest_flg);
-                // sofa有空, 允许customer从waiting room进入sofa, respond是阻塞的, 同时只能回应一个customer
-                msgsnd(sofa_respond_id, &msg_arg, sizeof(msg_arg), 0);
+            if (buff_ptr[ROOM] == 13) {
+                // 等待室没人
+                printf("新顾客 %d 直接坐在沙发上\n", id);
+                buff_ptr[SOFA]--;
+                // 请求理发
+                msg_arg.mtype = HAIRQUEST;
+                msgsnd(barber_quest_id, &msg_arg, sizeof(msg_arg), 0);
             } else {
-                // 等待室没人, customer直接进入sofa
-                printf("a new customer %d sit at sofa\n", id);
-                sofa_count++;
+                // 等待室有人, 新顾客先进入等待室, 然后再请求
+                printf("新顾客 %d 进入等待室等待进入沙发\n", id);
+                buff_ptr[ROOM]--;
+                msg_arg.mtype = SOFAQUEST;
+                msgsnd(sofa_quest_id, &msg_arg, sizeof(msg_arg), 0);
             }
-            // 需要更新沙发队列, 发送sofa customer对barber的quest
-            barber_quest_flg = IPC_NOWAIT;
-            // quest是非阻塞的, barber_quest一定按时间顺序排列
-            if (msgsnd(barber_quest_id, &msg_arg, sizeof(msg_arg), barber_quest_flg) < 0)
-                perror("message sent from sofa to barber failed.\n");
-        } else if (wait_count < 13){
-            // 等待室有空位, 沙发满
-            // customer只能进入等待室
-            printf("sofa is full, customer %d is waiting in the waiting room\n", id);
-            wait_count++;
-            // 发送customer对sofa的quest
-            sofa_quest_flg = IPC_NOWAIT;
-            // quest非阻塞, 发送quest, 这个quest在队列中一定按时间顺序排列了         
-            msgsnd(sofa_quest_id, &msg_arg, sizeof(msg_arg), sofa_quest_flg);
+        } else if (buff_ptr[ROOM] > 0){
+            // 沙发满了, 等待室有空位
+            printf("沙发满, 新顾客 %d 只能进入等待室等待\n", id);
+            buff_ptr[ROOM]--;
+            // 请求进入沙发
+            msg_arg.mtype = SOFAQUEST;
+            msgsnd(sofa_quest_id, &msg_arg, sizeof(msg_arg), 0);
         } else
             // 等待室和沙发都满了, customer离开
-            printf("waiting room is full, customer %d leaves\n", id);
+            printf("理发店满员, 顾客 %d 离开\n", id);
 
-        // 新customer安顿好了, 处理老customer
-        barber_respond_flg = IPC_NOWAIT;
-        // sofa接收barber发来的respond, 看是否理发结束, msgtype=0, 接收第1条消息即可, sofa上的人数--
-        if (msgrcv(barber_respond_id, &msg_arg, sizeof(msg_arg), 0, barber_respond_flg) > 0)
-            sofa_count--;
-        // waiting room接收sofa发来的respond, 看sofa是否有空, waiting room的人数--
-        sofa_respond_flg = IPC_NOWAIT;
-        if (msgrcv(sofa_respond_id, &msg_arg,sizeof(msg_arg), 0, sofa_respond_flg) > 0) {
-            printf("customer %d moves to sofa from the waiting room\n", msg_arg.mid);
-            wait_count--;
-            sofa_count++;
+        // 处理沙发请求
+        if (msgrcv(sofa_quest_id, &msg_arg, sizeof(msg_arg), SOFAQUEST, IPC_NOWAIT) >= 0) {
+            buff_ptr[SOFA]--;
+            buff_ptr[ROOM]++;
+            printf("顾客 %d 从等待室坐到沙发上\n", msg_arg.mid);
+            if (buff_ptr[CHAIR] > 0) {
+                msg_arg.mtype = HAIRQUEST;
+                msgsnd(barber_quest_id, &msg_arg, sizeof(msg_arg), 0);
+                printf("顾客 %d 请求理发\n", msg_arg.mid);
+            }
+        }
+        // 接收理发结束回应
+        if (msgrcv(barber_respond_id, &msg_arg, sizeof(msg_arg), CUT_FINISHED, IPC_NOWAIT) >= 0) {
+            printf("顾客 %d 理发完毕\n", msg_arg.mid);
+            msg_arg.mtype = ACCOUNTQUEST;
+            printf("顾客 %d 请求结账\n", msg_arg.mid);
+            msgsnd(account_quest_id, &msg_arg, sizeof(msg_arg), 0);
+        }
+        // 接收结账结束回应
+        if (msgrcv(account_respond_id, &msg_arg, sizeof(msg_arg), ACC_FINISHED, IPC_NOWAIT) >= 0) {
+            printf("顾客 %d 结账完毕\n", msg_arg.mid);
+            buff_ptr[CHAIR]++;
+            printf("顾客 %d 离开理发店\n", msg_arg.mid);
         }
     }
 
